809124aebc40f6f675ee25fd9852e00c796a53ed
luoar rian <rian.luoar@gmail.com>
2023-08-14 11:50:47 +0530

    v-html

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalA: '<b>Finish the course and learn Vue!</b>',
             courseGoalB: 'Master Vue',
             vueLink: 'https://vuejs.org/'
         };
